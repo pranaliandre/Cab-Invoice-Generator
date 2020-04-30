dd3d861dd34636a8b6e8db9265d238283d4b8798
pranaliandre <pranaliandre06@gmail.com>
2020-04-30 13:19:33 -0500

    Added: Welcome to cab invoice generator

---
+++ cabInvoiceGenerator/cabInvoiceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cabInvoiceGenerator
+{
+    class cabInvoiceGenerator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab Invoice Generator");
+        }
+    }
+}
